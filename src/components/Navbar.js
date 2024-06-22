23f7d9d63966bb0dd8e0e28a4aa420d7b31ddd60
MastanKhan1 <133966913+MastanKhan1@users.noreply.github.com>
2024-06-22 12:09:20 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,12 +19,12 @@ const Navbar = ()=>{
                     <h1 className="text-4xl font-extrabold">News</h1>
                 </div>
                 <div>
-                    <input type="text" className="mt-2 px-10 py-1 rounded-xl shadow-md ml-5" name="search" value={search} onChange={
+                    <input type="text" className="mt-2 px-10 py-1 rounded-xl shadow-md ml-2" name="search" value={search} onChange={
                          (e)=>{
                             setSearch(e.target.value)
                         }
                     }/>
-                    <button className="rounded-xl px-6 py-1 ml-5 text-white bg-black border-2 border-black font-semibold hover:shadow-md shadow-indigo-500" onClick={
+                    <button className="rounded-xl px-6 py-1 ml-3 text-white bg-black border-2 border-black font-semibold hover:shadow-md shadow-indigo-500" onClick={
                         ()=>{
                             setSearchedData(useFilteredData(search));
                         }
